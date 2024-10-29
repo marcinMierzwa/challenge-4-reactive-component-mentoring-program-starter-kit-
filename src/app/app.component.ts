311b58b0055b77edac58d3ef359d0fe91bb60fef
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-10-29 11:17:48 +0100

    update sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, inject, Signal } from '@angular/core';
+import { Component, computed, effect, inject, signal, Signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
@@ -20,17 +20,35 @@ export interface User {
 export class AppComponent {
   title = 'mentoring-program-starter-kit';
   private httpClient: HttpClient = inject(HttpClient);
-  users: Signal<User[]> = toSignal(this.httpClient
-    .get<any>('https://fakestoreapi.com/users')
-    .pipe(
+  users: Signal<User[]> = toSignal(
+    this.httpClient.get<any>('https://fakestoreapi.com/users').pipe(
       map((users) =>
-        users.map((user: { email: string; name: { firstname: string; lastname: string; }; }) => ({
-          email: user.email,
-          firstname: user.name.firstname, 
-          lastname: user.name.lastname,
-        }))
-      ))
-      , { initialValue: [] }
-    )
-}
+        users.map(
+          (user: {
+            email: string;
+            name: { firstname: string; lastname: string };
+          }) => ({
+            email: user.email,
+            firstname: user.name.firstname,
+            lastname: user.name.lastname,
+          })
+        ))), { initialValue: [] }
+  );
 
+  sortOrder: WritableSignal<string> = signal('asc');
+  visibleUsers: Signal<User[]> = computed( () => {
+    return this.users().sort((a: User, b: User) => {
+      return this.sortOrder() === 'asc'
+      ? a.email.localeCompare(b.email)
+      : b.email.localeCompare(a.email)
+    });
+  });
+
+
+
+  constructor() {
+    effect(() => console.log(this.visibleUsers())
+    )
+    
+  }
+}
